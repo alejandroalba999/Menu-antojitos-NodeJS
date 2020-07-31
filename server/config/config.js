8d814bf66da4fb600bd3cedc0e2ef49f66171d0b
USUARIO <alejandroalba999@gmail.com>
2020-07-31 12:09:21 -0500

    carpetas iniciales

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
-const permiso = require('../middlewares/permisos');
+
 process.env.PORT = process.env.PORT || 3000;
 //Entorno
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
@@ -6,16 +6,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     log = true;
-    urlDB = 'mongodb://localhost:27017/ruta'; //mongodb://localhost:27017/ruta
+    urlDB = 'mongodb://localhost:27017/menu'; //mongodb://localhost:27017/ruta
 } else {
-    urlDB = 'mongodb+srv://admin:admin@cluster0-rzrls.mongodb.net/ruta';
-    log= false;
+    urlDB = '';
+    
 }
-process.middlewares = [permiso.rolMenuUsuario];
-process.env.URLDB = urlDB;
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-//FIRMA DE JWT
-process.env.SEED = process.env.SEED || 'firma-super-secreta';
-
-//EXPIRE TIME JWT
-process.env.CADUCIDAD_TOKEN = process.env.CADUCIDAD_TOKEN || '3h';
